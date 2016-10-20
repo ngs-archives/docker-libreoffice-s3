45e3274ed0406dd4787425c6f94ac48458fb0737
Atsushi Nagase <a@ngs.io>
2016-10-21 08:14:38 +0900

    Remove unused shell script

--- convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-lowriter --invisible --convert-to pdf:writer_pdf_Export --outdir /var/files /var/files/test.pptx
-curl -XPOST http://requestb.in/130mvcp1 -H 'Content-Type: text/plain' -d $(ls -lsa /var/files)
